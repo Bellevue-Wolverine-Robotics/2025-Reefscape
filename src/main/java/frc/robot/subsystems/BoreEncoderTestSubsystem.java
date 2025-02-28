b9bf01aaa3a41fc772bcdc885ed79d6617472487
ilovetocode <54194229+ilovetocode2019@users.noreply.github.com>
2025-02-27 19:27:10 -0800

    Remove irrelevant items

--- src/main/java/frc/robot/subsystems/BoreEncoderTestSubsystem.java
+++ src/main/java/frc/robot/subsystems/BoreEncoderTestSubsystem.java
@@ -1,41 +1,17 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
 
-import edu.wpi.first.wpilibj.CounterBase;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class BoreEncoderTestSubsystem extends SubsystemBase {
-    private final SparkMax leftPulleyMotor;
-    private final SparkMax rightPulleyMotor;
-
-    // These nums are temporary and meaningless
-    public static final double L1 = 25.0d;
-    public static final double L2 = 55.0d;
-    public static final double L3 = 75.0d;
-    public static final double L4 = 101.0d;
-
-    public static final double RolloverThreshold = 0.75d;
-
+    private static final double RolloverThreshold = 0.75d;
+    private double position = 0.0d;
     private double movementDir;
     private double rotation;
-    private double speed = 0.1d;
-    private double position = 0.0d;
-    private double targetHeight = 0.0d;
-
     private final DutyCycleEncoder dutyCycleEncoder;
-    private int i = 0;
 
     public BoreEncoderTestSubsystem() {
-        leftPulleyMotor = new SparkMax(4, MotorType.kBrushless);
-        rightPulleyMotor = new SparkMax(2, MotorType.kBrushless);
-
-        leftPulleyMotor.setInverted(true);
-        rightPulleyMotor.setInverted(true);
-        
         dutyCycleEncoder = new DutyCycleEncoder(0);
     }
 
@@ -43,38 +19,22 @@ public class BoreEncoderTestSubsystem extends SubsystemBase {
         return dutyCycleEncoder.get();
     }
 
-    public void goToHeight(double height) {
-        movementDir = Math.signum(height - position);
-        setBothMotors(speed * movementDir);
-        targetHeight = height;
-    }
-
     @Override
     public void periodic() {
-        IncreaseDist();
-        rotation = getCurrentRotation();
-        System.out.println(position);
+        UpdatePosition();
     }
 
-    private void setBothMotors(double speed)
-    {
-        leftPulleyMotor.set(speed);
-        rightPulleyMotor.set(speed);
-    }
-
-    private void IncreaseDist()
-    {
+    private void UpdatePosition() {
         var previousRotation = rotation;
         var currentRotation = getCurrentRotation();
         var change = currentRotation - previousRotation;
         var sign = Math.signum(change);
 
-        if (Math.abs(change) >= RolloverThreshold)
-        {
+        if (Math.abs(change) >= RolloverThreshold) {
             change = (1 - (Math.abs(change))) * -sign;
-            //System.out.println("Previous: " + rotation + ", Current: " + currentRotation + ", Change: " + change);
         }
 
         position += change;
+        rotation = currentRotation;
     }
 }
