21a5fbc5d8717bb93cc204e3dd9a9ef106d4684e
Dylhouse <134633294+Dylhouse@users.noreply.github.com>
2025-03-01 15:21:28 -0800

    Cleaned up stuff, also initial commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,3 +1,4 @@
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
@@ -54,8 +55,11 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
+
+    //m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
