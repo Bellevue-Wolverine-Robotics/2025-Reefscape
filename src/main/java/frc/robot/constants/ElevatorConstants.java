9cbde2a9e8c7eb7646bcd9104ec1002d9d557b72
Dylhouse <134633294+Dylhouse@users.noreply.github.com>
2025-03-01 10:14:27 -0800

    Changed elevator subsystem

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -2,6 +2,10 @@ package frc.robot.constants;
 
 public final class ElevatorConstants {
     public static final double ROLLOVER_THRESHOLD = 0.75d;
+    public static final double BOTTOM_POSITION = 0.0d;
+    // change this later
+    public static final double TOP_POSITION = 100.0d;
+
     public static final int DUTY_CYCLE_ENCODER_PORT = 0;
     public static final int BOTTOM_LIMIT_SWITCH_PORT = 2;
     public static final int TOP_LIMIT_SWITCH_PORT = 1;
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -15,28 +15,47 @@ public class ElevatorSubsystem extends SubsystemBase {
     private final SparkMax leftMotor = new SparkMax(ElevatorConstants.LEFT_MOTOR_ID, MotorType.kBrushless);
     private final SparkMax rightMotor = new SparkMax(ElevatorConstants.RIGHT_MOTOR_ID, MotorType.kBrushless);
 
+    private double speed = 0.03d;
+
     private double position = 0.0d;
     private double previousRotation;
 
     private double targetPosition = 0;
+    // false is down, true is up
     private boolean targetDirection = false;
 
-    public void setTargetPosition(int position) {
-        targetPosition = position;
-        targetDirection = position - position > 0;
+    /**
+     * Sets the target position of the elevator, aka where it is trying to go to.
+     * @param targetPosition The position the elevator will start moving to.
+     * @exception 
+     */
+    public void setTargetPosition(int targetPosition) {
+        // check targetPosition is in range
+        if (targetPosition < ElevatorConstants.BOTTOM_POSITION || targetPosition > ElevatorConstants.TOP_POSITION) return;
+        this.targetPosition = targetPosition;
+        targetDirection = targetPosition - position > 0;
     }
 
     @Override
     public void periodic() {
         UpdatePosition();
 
-        if (targetPosition - position > 0 != targetDirection || !bottomLimitSwitch.get() || !topLimitSwitch.get()) {
+        if (bottomLimitSwitch.get()) {
+            position = ElevatorConstants.BOTTOM_POSITION;
+        }
+        else if (topLimitSwitch.get()) {
+            position = ElevatorConstants.TOP_POSITION;
+        }
+
+        
+        var movementDirection = targetPosition - position > 0;
+        if (movementDirection != targetDirection) {
             leftMotor.stopMotor();
             rightMotor.stopMotor();
         } else {
-            double speed = targetPosition > 0 ? 0.01 : -0.01;
-            leftMotor.set(speed);
-            rightMotor.set(speed);
+            double velocity = movementDirection ? speed : -speed;
+            leftMotor.set(velocity);
+            rightMotor.set(velocity);
         }
     }
 
