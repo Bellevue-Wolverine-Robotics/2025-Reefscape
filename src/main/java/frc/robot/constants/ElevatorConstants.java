831b8ad820a9e227330e4008edfcecfa8534ad86
Joseph An <52297896+enyoukai@users.noreply.github.com>
2025-03-08 10:50:54 -0800

    Update elevator constants

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -4,7 +4,7 @@ public final class ElevatorConstants {
     public static final double CIRCUMFERENCE = Math.PI  * 0.75d; // 0.75" is the diameter, circumference = 2d
     public static final double DISTANCE_PER_PULSE = CIRCUMFERENCE / 2048.0d; // 2048 is the bore encoder resolution (cycles per rev)
 
-    public static final int MOTOR_ID = 30;
+    public static final int MOTOR_ID = 31;
     public static final int LIMIT_SWITCH_PORT = 3;
     public static final int ENCODER_CHANNEL_A = 1;
     public static final int ENCODER_CHANNEL_B = 2;
@@ -16,10 +16,10 @@ public final class ElevatorConstants {
 
     public static final double LEVEL_ONE = 2.0d;
     public static final double LEVEL_TWO = 3.0d;
-    public static final double LEVEL_THREE = 4.0d;
+    public static final double LEVEL_THREE = 6.0d;
     public static final double LEVEL_FOUR = 5.0d;
     public static final double INTAKE_LEVEL = 1.0d;
     public static final double BOTTOM_LEVEL = 0.0d;
 
-    public static final double INCREMENT_DISTANCE = 1.0d;
+    public static final double INCREMENT_DISTANCE = 1.5d;
 }
